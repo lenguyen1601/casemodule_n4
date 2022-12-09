1229927f569637e87c1f4db5e04dc350dd9e774b
lenguyen1601 <110454396+lenguyen1601@users.noreply.github.com>
2022-12-09 09:54:46 +0700

    upproject

---
+++ src/main/java/com/example/case_module_n4/CaseModuleN4Application.java
@@ -0,0 +1,13 @@
+package com.example.case_module_n4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseModuleN4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseModuleN4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/case_module_n4/CaseModuleN4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.case_module_n4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseModuleN4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
